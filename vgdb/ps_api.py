ed69b615d64d0eba91965ba2d1f931ed5e1a3d88
Ryder McMinn <mcminnra@gmail.com>
2022-11-23 13:20:50 -0800

    ps async

--- vgdb/ps_api.py
+++ vgdb/ps_api.py
@@ -1,3 +1,4 @@
+from concurrent.futures import as_completed
 from datetime import datetime
 import json
 import time
@@ -6,6 +7,7 @@ from urllib.parse import parse_qs
 
 import numpy as np
 import requests
+from requests_futures.sessions import FuturesSession
 from tqdm import tqdm
 
 
@@ -15,6 +17,7 @@ class PlaystationClient():
         self.npsso = npsso
         self.access_token = None
 
+        self.session = FuturesSession()
         self.WAIT_TIME = 0.5
 
         if not self.access_token:
@@ -55,10 +58,10 @@ class PlaystationClient():
         return access_token
 
     def get_played_titles(self):
-        # Get titles
-        headers = {
-            "Authorization": f"Bearer {self.access_token}"
-        }
+        # == Get titles
+        start = time.time()
+        print('Playstation Played Titles...')
+        headers = {"Authorization": f"Bearer {self.access_token}"}
         r = requests.get(
             "https://m.np.playstation.com/api/gamelist/v2/users/me/titles?categories=ps4_game,ps5_native_game&limit=250&offset=0",
             headers=headers
@@ -87,33 +90,6 @@ class PlaystationClient():
             title['first_played'] = datetime.strptime(title['first_played'], "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()
             title['last_played'] = datetime.strptime(title['last_played'], "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()
 
-        # Get trophy information
-        for title in tqdm(titles, desc='Playstation Trophies'):
-            headers = {
-                "Authorization": f"Bearer {self.access_token}"
-                }
-            r = requests.get(
-                f'https://m.np.playstation.com/api/trophy/v1/users/me/titles/trophyTitles?npTitleIds={title["ps_np_title_id"]}',
-                headers=headers
-            )
-            time.sleep(self.WAIT_TIME)
-            # NOTE: In the future, you can supply multiple np_title_ids => npTitleIds={",".join(query_np_title_ids)} of maybe max 5 ids
-            trophy_title = json.loads(r.text)['titles'][0]
-
-            if len(trophy_title['trophyTitles']) > 0:
-                title['ps_np_comm_id'] = trophy_title['trophyTitles'][0]['npCommunicationId']
-                title['trophy_weighted_progress'] = trophy_title['trophyTitles'][0]['progress']  # Can there be multiple trophy sets per npTitleId?
-                title['completed_trophies'] = \
-                    int(trophy_title['trophyTitles'][0]["earnedTrophies"]['bronze']) +\
-                    int(trophy_title['trophyTitles'][0]["earnedTrophies"]['silver']) +\
-                    int(trophy_title['trophyTitles'][0]["earnedTrophies"]['gold']) +\
-                    int(trophy_title['trophyTitles'][0]["earnedTrophies"]['platinum'])
-                title['total_trophies'] = \
-                    int(trophy_title['trophyTitles'][0]["definedTrophies"]['bronze']) +\
-                    int(trophy_title['trophyTitles'][0]["definedTrophies"]['silver']) +\
-                    int(trophy_title['trophyTitles'][0]["definedTrophies"]['gold']) +\
-                    int(trophy_title['trophyTitles'][0]["definedTrophies"]['platinum'])
-
         # Process playtime into a float
         for title in titles:
             playtime_str = title['playtime']
@@ -134,5 +110,65 @@ class PlaystationClient():
             playtime_minutes = playtime_hours*60
 
             title['playtime'] = np.round(playtime_minutes, 1)
+        print(f'Playstation Played Titles [{time.time()-start:.2f} seconds]')
+
+        #== Get trophy information
+        start = time.time()
+        print('Playstation Played Titles Trophies...')
+        titles = self._enrich_with_trophies(titles)
+        print(f'Playstation Played Titles Trophies [{time.time()-start:.2f} seconds]')
 
         return titles
+
+    def _enrich_with_trophies(self, titles):
+        """
+        Async enriches records list with trophies data
+        """
+        futures=[]
+        for title in titles:
+            headers = {"Authorization": f"Bearer {self.access_token}"}
+            # NOTE: In the future, you can supply multiple np_title_ids => npTitleIds={",".join(query_np_title_ids)} of maybe max 5 ids
+            future = self.session.get(
+                f'https://m.np.playstation.com/api/trophy/v1/users/me/titles/trophyTitles?npTitleIds={title["ps_np_title_id"]}',
+                headers=headers
+            )
+            future.title = title
+            futures.append(future)
+
+        titles_with_trophy_data = []
+        for future in as_completed(futures):
+            try:
+                # Get response
+                resp = future.result()
+                title = future.title
+                trophy_json = resp.json()['titles'][0]
+
+                # Handle status
+                if resp.status_code > 299:
+                    raise Exception
+
+                # Process trophies
+                if len(trophy_json['trophyTitles']) > 0:
+                    title['ps_np_comm_id'] = trophy_json['trophyTitles'][0]['npCommunicationId']
+                    title['trophy_weighted_progress'] = trophy_json['trophyTitles'][0]['progress']  # Can there be multiple trophy sets per npTitleId?
+                    title['completed_trophies'] = \
+                        int(trophy_json['trophyTitles'][0]["earnedTrophies"]['bronze']) +\
+                        int(trophy_json['trophyTitles'][0]["earnedTrophies"]['silver']) +\
+                        int(trophy_json['trophyTitles'][0]["earnedTrophies"]['gold']) +\
+                        int(trophy_json['trophyTitles'][0]["earnedTrophies"]['platinum'])
+                    title['total_trophies'] = \
+                        int(trophy_json['trophyTitles'][0]["definedTrophies"]['bronze']) +\
+                        int(trophy_json['trophyTitles'][0]["definedTrophies"]['silver']) +\
+                        int(trophy_json['trophyTitles'][0]["definedTrophies"]['gold']) +\
+                        int(trophy_json['trophyTitles'][0]["definedTrophies"]['platinum'])
+                else:
+                    print(f'No trophies for [{title["ps_np_title_id"]}] {title["title"]}')
+
+                titles_with_trophy_data.append(title)
+
+            except:
+                print(f'[{resp.status_code}] on {future.title["title"]}')
+                import sys; sys.exit(1)
+
+        return titles_with_trophy_data
+            
